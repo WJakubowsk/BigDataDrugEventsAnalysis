2af96bd73bbb803c8b1fd17f2489e65bee5ffac4
Wiktor Jakubowski <wiktor.jak97@gmail.com>
2023-12-23 23:54:07 +0000

    add save directly to hdfs

--- scripts/get_events_from_api.sh
+++ scripts/get_events_from_api.sh
@@ -7,15 +7,15 @@ end_date=$(date +"%Y%m%d")
 current_date=$start_date
 
 while [ $start_date -lt $end_date ]; do
-    filename="/home/vagrant/project/BigDataProject/data/api/${current_date}.json"
+    filename="/home/vagrant/project/BigDataProject/data/apii/${current_date}.json"
 
     url="https://api.fda.gov/drug/event.json?api_key=SYDGOVhKmq00Zdk2CI1bedQaki7xf4bsiGR8ZDVo&search=receivedate:\[${current_date}+TO+${current_date}\]&limit=1000"
 
-    response=$(curl -k --compressed -X GET "$url" | jq '{ results: .results }')
+    response=$(curl -s -k --compressed -X GET "$url" | jq '{ results: .results }')
 
     # Check if the "results" section is not empty before saving
     if [ ! -z "$response" ]; then
-        echo "$response" > "$filename"
+        echo "$response" | hdfs dfs -put - "/user/vagrant/project/nifi_in/api/${current_date}.json"
     fi
 
     current_date=$(date -d "$current_date + 1 day" +%Y%m%d)
