a5a772394127ce336aea634634f03aee32add52b
Wiktor Jakubowski <wiktor.jak97@gmail.com>
2023-12-23 00:20:34 +0000

    tbd: update insert into hive

--- scripts/preprocess_products.py
+++ scripts/preprocess_products.py
@@ -18,6 +18,7 @@ with hdfs_client.read(hdfs_csv_path) as reader:
     df = pd.read_csv(reader, sep=";")
 
 df_filtered = df.sort_values('ProductNo').drop_duplicates(subset = 'ApplNo', keep='first')
+df_filtered['DrugName'] = df_filtered['DrugName'].str.replace(',', ';')
 
 df_filtered.to_csv(local_preprocessed_csv_path, index = False)
 
@@ -27,7 +28,7 @@ df_filtered.to_csv(local_preprocessed_csv_path, index = False)
     # writer.write(df_filtered)
     
 
-conn = hive.Connection(host=hive_host, port=hive_port, auth='NOSASL', database='default')
+conn = hive.Connection(host=hive_host, port=hive_port, auth=None, database='default')
 cursor = conn.cursor()
 
 create_table_query = f'''
@@ -42,14 +43,43 @@ CREATE TABLE IF NOT EXISTS {hive_table_name} (
     reference_standard INT
 )
 '''
+cursor.execute("USE default")
 cursor.execute(create_table_query)
 
-table_name_with_db = f'default.{hive_table_name}'
-cursor.execute(f"USE default")
-cursor.execute(f"DROP TABLE IF EXISTS {table_name_with_db}")
-cursor.execute(f"CREATE TABLE {table_name_with_db} (LIKE {hive_table_name})")
-cursor.execute(f"INSERT INTO TABLE {table_name_with_db} SELECT * FROM {hive_table_name}")
+df_mapped = df_filtered.rename(columns={
+    'ApplNo': 'application_number',
+    'ProductNo': 'product_number',
+    'Form': 'drug_administration_form',
+    'Strength': 'drug_strength',
+    'ReferenceDrug': 'reference_drug',
+    'DrugName': 'drug_name',
+    'ActiveIngredient': 'active_ingredient',
+    'ReferenceStandard': 'reference_standard'
+    })
 
+# df_mapped.fillna(value='NULL', inplace=True) #TODOOO missing values -> NULL, nie nan/0
+
+str_cols = ['drug_administration_form', 'drug_strength', 'drug_name', 'active_ingredient']
+int_cols = ['application_number', 'product_number', 'reference_drug', 'reference_standard']
+for col in df_mapped.columns:
+    format = str if col in str_cols else int
+    df_mapped.loc[df_mapped[col].notna(), col] = df_mapped.loc[df_mapped[col].notna(), col].astype(format)
+
+
+columns_str = ', '.join(df_mapped.columns)
+placeholders_str = ', '.join(['?'] * len(df_mapped.columns))  # Using '?' as placeholders for values
+
+# Prepare the INSERT INTO query with explicit column names
+insert_query = f"INSERT INTO {hive_table_name} ({columns_str}) VALUES ({placeholders_str})"
+
+# Convert DataFrame rows to tuples for insertion
+rows_to_insert = [tuple(row) for row in df_mapped.values]
+
+# Execute insertion query for each row
+for row in rows_to_insert:
+    cursor.execute(insert_query, row)
+
+conn.commit()
 cursor.close()
 conn.close()
 
