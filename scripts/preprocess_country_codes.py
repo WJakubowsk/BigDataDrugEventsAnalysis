3ef9331d7823684e677bb5fbfa7183a5428cd9cf
Wiktor Jakubowski <wiktor.jak97@gmail.com>
2023-12-23 21:14:09 +0000

    add loading to hive

---
+++ scripts/preprocess_country_codes.py
@@ -0,0 +1,49 @@
+from pyhive import hive
+import pandas as pd
+from hdfs import InsecureClient
+
+# Set up connection details
+hive_host = 'localhost'
+hive_port = 10000
+hdfs_host = 'node1'
+hdfs_port = 50070
+hdfs_csv_path = '/user/vagrant/project/nifi_in/country_codes.csv'
+hive_table_name = 'country_codes'
+
+hdfs_client = InsecureClient(f'http://{hdfs_host}:{hdfs_port}', user = "vagrant")
+
+with hdfs_client.read(hdfs_csv_path) as reader:
+    df = pd.read_csv(reader, sep=",")
+    
+
+conn = hive.Connection(host=hive_host, port=hive_port, auth=None, database='default')
+cursor = conn.cursor()
+
+create_table_query = f'''
+CREATE TABLE IF NOT EXISTS {hive_table_name} (
+    country VARCHAR(100),
+    code VARCHAR(100)
+)
+'''
+
+cursor.execute("USE default")
+cursor.execute(create_table_query)
+
+df_mapped = df.rename(columns={
+    'Name': 'country',
+    'Code': 'code'
+    })
+
+columns_str = ', '.join(df_mapped.columns)
+
+
+insert_query = f"INSERT INTO {hive_table_name} ({columns_str}) VALUES (%s, %s)"
+
+rows_to_insert = [tuple(row) for row in df_mapped.values]
+
+for row in rows_to_insert:
+    cursor.execute(insert_query, row)
+
+conn.commit()
+cursor.close()
+conn.close()
--- scripts/preprocess_products.py
+++ scripts/preprocess_products.py
@@ -2,15 +2,14 @@ from pyhive import hive
 import pandas as pd
 from hdfs import InsecureClient
 
-# Set up connection details
 hive_host = 'localhost'
 hive_port = 10000
 hdfs_host = 'node1'
 hdfs_port = 50070
 hdfs_csv_path = '/user/vagrant/project/nifi_in/Products.csv'
-hdfs_preprocessed_csv_path = '/user/vagrant/project/nifi_in/preprocessed_Drugs.csv'
+hdfs_preprocessed_csv_path = '/user/vagrant/project/nifi_in/preprocessed_drugs.csv'
 local_preprocessed_csv_path = '/home/vagrant/project/BigDataProject/data/preprocessed_Drugs.csv'
-hive_table_name = 'Drugs'
+hive_table_name = 'drugs'
 
 hdfs_client = InsecureClient(f'http://{hdfs_host}:{hdfs_port}', user = "vagrant")
 
@@ -18,15 +17,12 @@ with hdfs_client.read(hdfs_csv_path) as reader:
     df = pd.read_csv(reader, sep=";")
 
 df_filtered = df.sort_values('ProductNo').drop_duplicates(subset = 'ApplNo', keep='first')
+df_filtered['Form'] = df_filtered['Form'].str.replace(',', ';')
 df_filtered['DrugName'] = df_filtered['DrugName'].str.replace(',', ';')
 
 df_filtered.to_csv(local_preprocessed_csv_path, index = False)
 
-# hdfs_client.upload('/user/vagrant/project/nifi_in/', local_preprocessed_csv_path)
-
-# with hdfs_client.write(hdfs_preprocessed_csv_path, encoding='utf-8') as writer:
-    # writer.write(df_filtered)
-    
+hdfs_client.upload(hdfs_preprocessed_csv_path, local_preprocessed_csv_path)
 
 conn = hive.Connection(host=hive_host, port=hive_port, auth=None, database='default')
 cursor = conn.cursor()
@@ -35,12 +31,10 @@ create_table_query = f'''
 CREATE TABLE IF NOT EXISTS {hive_table_name} (
     application_number INT,
     product_number INT,
-    drug_administration_form STRING,
-    drug_strength STRING,
-    reference_drug INT,
-    drug_name STRING,
-    active_ingredient STRING,
-    reference_standard INT
+    drug_administration_form VARCHAR(100),
+    drug_strength VARCHAR(100),
+    drug_name VARCHAR(100),
+    active_ingredient VARCHAR(100)
 )
 '''
 cursor.execute("USE default")
@@ -51,36 +45,28 @@ df_mapped = df_filtered.rename(columns={
     'ProductNo': 'product_number',
     'Form': 'drug_administration_form',
     'Strength': 'drug_strength',
-    'ReferenceDrug': 'reference_drug',
     'DrugName': 'drug_name',
     'ActiveIngredient': 'active_ingredient',
-    'ReferenceStandard': 'reference_standard'
-    })
-
-# df_mapped.fillna(value='NULL', inplace=True) #TODOOO missing values -> NULL, nie nan/0
+    }).drop(['ReferenceDrug', 'ReferenceStandard'], axis = 1)
 
 str_cols = ['drug_administration_form', 'drug_strength', 'drug_name', 'active_ingredient']
-int_cols = ['application_number', 'product_number', 'reference_drug', 'reference_standard']
-for col in df_mapped.columns:
-    format = str if col in str_cols else int
-    df_mapped.loc[df_mapped[col].notna(), col] = df_mapped.loc[df_mapped[col].notna(), col].astype(format)
+int_cols = ['application_number', 'product_number']
+
+df_mapped[str_cols] = df_mapped[str_cols].fillna(value='missing')
+df_mapped[int_cols] = df_mapped[int_cols].fillna(value=-1) # application and product numbers are poisitve
 
 
 columns_str = ', '.join(df_mapped.columns)
-placeholders_str = ', '.join(['?'] * len(df_mapped.columns))  # Using '?' as placeholders for values
 
-# Prepare the INSERT INTO query with explicit column names
+placeholders_str = ', '.join(['%s'] * len(df_mapped.columns))
+
 insert_query = f"INSERT INTO {hive_table_name} ({columns_str}) VALUES ({placeholders_str})"
 
-# Convert DataFrame rows to tuples for insertion
 rows_to_insert = [tuple(row) for row in df_mapped.values]
 
-# Execute insertion query for each row
 for row in rows_to_insert:
     cursor.execute(insert_query, row)
 
 conn.commit()
 cursor.close()
 conn.close()
-
-print("Data loaded into Hive table successfully!")
