8336fe2e88e233a87e3226245280e5a596fcb349
Wiktor Jakubowski <wiktor.jak97@gmail.com>
2023-12-25 17:59:16 +0000

    update python version

--- scripts/preprocess_api_drug_events.sh
+++ scripts/preprocess_api_drug_events.sh
@@ -3,4 +3,4 @@
 . /home/vagrant/miniconda3/etc/profile.d/conda.sh
 conda activate bigdata
 
-python3.9 /home/vagrant/project/BigDataProject/scripts/preprocess_api_drug_events.py
+python /home/vagrant/project/BigDataProject/scripts/preprocess_api_drug_events.py
--- scripts/preprocess_country_codes.sh
+++ scripts/preprocess_country_codes.sh
@@ -3,4 +3,4 @@
 . /home/vagrant/miniconda3/etc/profile.d/conda.sh
 conda activate bigdata
 
-python3.9 /home/vagrant/project/BigDataProject/scripts/preprocess_country_codes.py
+python /home/vagrant/project/BigDataProject/scripts/preprocess_country_codes.py
